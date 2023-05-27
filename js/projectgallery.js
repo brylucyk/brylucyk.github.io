c214404d0fb188553d4a01d184e668dca7971977
Bryanna Lucyk <brylucyk@gmail.com>
2023-05-27 17:52:45 -0400

    re-added garden planner and changed gallery sizing

--- js/projectgallery.js
+++ js/projectgallery.js
@@ -91,7 +91,7 @@ const projects = {
         "title": "Garden Planner",
         "description": "I made this to learn Angular, Material Design & TypeScript. Also, I needed a little app to plan my garden.",
         "architecture": "Angular, Material Design, TypeScript, JSON server for local data storage",
-        "web": "",
+        "web": "7",
         "mobile": 0,
         "ext": ".png"
     }
@@ -158,10 +158,9 @@ function populateOverlay(imageLocation) {
 
     for (let i = 0; i < projects[project[1]]["mobile"]; i++) {
         const $image = $(`<img src="./images/screenshots/${project[1]}_recording_mobile_${i + 1}${projects[project[1]]["ext"]}" class="mobile-screenshot">`);
-        $mobileContainer.append($image);
+        $main.append($image);
     }
 
-    $main.append($mobileContainer);
 }
 
 // When next button is clicked
